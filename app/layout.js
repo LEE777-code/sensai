4b15ac34bce250cb9cc9e1feff89e76bdbf849ea
LEE777-code <ishanthleeman@gmail.com>
2025-09-21 08:38:29 +0530

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -37,7 +37,7 @@ export default function RootLayout({ children }) {
 
             <footer className="bg-muted/50 py-12">
               <div className="container mx-auto px-4 text-center text-gray-200">
-                <p>Made By Ishanth Leeman</p>
+                <p>Made By Friendly Frameworks</p>
               </div>
             </footer>
           </ThemeProvider>
